a6b9e59a8188c6ec6f92e02d6ad9a16f47461169
Landy <liutianxuanlandy@gmail.com>
2023-05-17 15:42:57 +0200

    lab sql 2.6 updated after completion.

--- lab-sql_2.6.sql
+++ lab-sql_2.6.sql
@@ -2,8 +2,8 @@
 use sakila;
 
 -- 1. In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
-SELECT last_name, count(*) freq FROM actor
-GROUP BY last_name
+SELECT first_name, last_name, count(*) freq FROM actor
+GROUP BY first_name, last_name
 HAVING freq = 1;
 
 -- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
